39dfc7144fb93bb04ce84519996f56bba23ea479
Sofia Serrano <syserrano@usf.edu>
2026-01-31 11:34:24 -0500

    Added reorder level, target stock, weekly demand and supplier lead time variables.

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -7,3 +7,15 @@ let unitCost = 8.88;
 // how much stock is available
 let currentStock = 70;
 
+// threshsold to consider reordering
+let reorderLevel = 50;
+
+// desired on-hand after reorder
+let targetStock = 500;
+
+// units sold per week
+let weeklyDemand = 200;
+
+// weeks to receive an order
+let supplierLeadTimeWeeks = 2;
+
